5092ddbc2d7c4e678abca29298bd93752ee928de
Ritik Yadav <96940755+Ritik-Yadav07@users.noreply.github.com>
2023-06-11 10:16:57 +0530

    Added solution - LeetHub

---
+++ Find all pairs with a given sum - GFG/find-all-pairs-with-a-given-sum.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        // Your code goes here 
+        vector<pair<int,int>>res;
+        unordered_map<int,int>mp; //val-index
+        for(int i = 0;i<N;i++)
+        {
+            mp[A[i]] = i;
+        }
+        
+        for(int i=0;i<M;i++)
+        {
+            int valueTobeSearched = X-B[i];
+            auto it = mp.find(valueTobeSearched );
+            
+            if(it!=mp.end())
+            {
+                //i.e. we have found such pair
+                res.push_back({valueTobeSearched,B[i]});
+            }
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
+// } Driver Code Ends
