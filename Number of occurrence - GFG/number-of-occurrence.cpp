74ff7ae64bc2ba3619b4c73f0c3bc3f0545247b2
Ritik Yadav <96940755+Ritik-Yadav07@users.noreply.github.com>
2023-08-20 09:28:10 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,50 +10,63 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
+	int firstOccurance(int arr[],int n,int x)
+	{
+	    int ind = -1;
+	    
+	    int start=0;
+	    int end = n-1;
+	    
+	    while(start<=end){
+	        int mid = start + (end-start)/2;
+	        
+	        if(arr[mid]==x){
+	            ind = mid;
+	            end = mid-1;
+	        }
+	        else if(arr[mid]>x){
+	            end = mid-1;
+	        }
+	        else{
+	            start = mid+1;
+	        }
+	    }
+	    
+	    return ind;
+	}
+	int lastOccurance(int arr[],int n,int x)
+	{
+	    int ind = -1;
+	    
+	    int start=0;
+	    int end = n-1;
+	    
+	    while(start<=end){
+	        int mid = start + (end-start)/2;
+	        
+	        if(arr[mid]==x){
+	            ind = mid;
+	            start = mid+1;
+	        }
+	        else if(arr[mid]>x){
+	            end = mid-1;
+	        }
+	        else{
+	            start = mid+1;
+	        }
+	    }
+	    
+	    return ind;
+	}
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int first = -1;
-    int last = -1;
-    
-    int low = 0;
-    int high = n-1;
-    
-    while(low<=high)
-    {
-        int mid = low + (high-low)/2;
-        
-        if(arr[mid]==x)
-        {
-            first = mid;
-            high = mid-1;
-        }
-        else if(arr[mid]>x)
-        {
-            high = mid-1;
-        }
-        else low  = mid+1;
-    }
-    
-    low = 0;
-    high = n-1;
-    
-    while(low<=high)
-    {
-        int mid = low + (high-low)/2;
-        
-        if(arr[mid]==x)
-        {
-            last = mid;
-            low = mid+1;
-        }
-        else if(arr[mid]>x)
-        {
-            high = mid-1;
-        }
-        else low  = mid+1;
-    }
-    if(first ==-1 || last==-1) return 0;
-    return last-first+1;
+	    int firstPos = firstOccurance(arr,n,x);  //0 based indexs
+	    int lastPos = lastOccurance(arr,n,x);
+	    
+	    if(firstPos==-1 || lastPos==-1) return 0; // x is not present
+	    
+	    return (lastPos-firstPos)+1;
+	    
 	}
 };
 
