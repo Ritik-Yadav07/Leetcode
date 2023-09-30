65a5cc3c3f684a4724dc27661ef03932977c01cb
Ritik Yadav <96940755+Ritik-Yadav07@users.noreply.github.com>
2023-09-30 13:10:59 +0530

    Added solution - LeetHub

--- Boolean Matrix - GFG/boolean-matrix.cpp
+++ Boolean Matrix - GFG/boolean-matrix.cpp
@@ -10,79 +10,47 @@ class Solution
     public:
     //Function to modify the matrix such that if a matrix cell matrix[i][j]
     //is 1 then all the cells in its ith row and jth column will become 1.
-    void booleanMatrix(vector<vector<int> > &m)
+    void booleanMatrix(vector<vector<int> > &matrix)
     {
         // code here 
-        // int row=matrix.size();
-        // int col=matrix[0].size();
+        int r = matrix.size();
+        int c = matrix[0].size();
         
-        // int dummyCol[col];
-        // int dummyRow[row];
-        // for(int i=0;i<row;i++){
-        //     for(int j=0;j<col;j++){
-        //         if(matrix[row][col]==1){
-        //             dummyCol[j]=1;
-        //             dummyRow[i]=1;
-        //         }
-        //     }
-        // }
+        vector<int>RowVector(r,0);
+        vector<int>ColVector(c,0);
         
-        // for(int i=0;i<row;i++){
-        //   for(int j=0;j<col;j++){
-        //       if(dummyRow[i]==1 || dummyCol[j]==1){
-        //           matrix[i][j]=1;
-        //       }
-        //   }
-        // }
-         int n1=m.size();
-
-        int n2=m[0].size();
-
-        int row[n1],col[n2];
-
-        for(int i=0;i<n1;i++)
-
+        for(int i=0;i<r;i++)
         {
-
-            for(int j=0;j<n2;j++)
-
+            for(int j = 0;j<c;j++)
             {
-
-                if(m[i][j]==1)
-
-                {
-
-                    row[i]=1;
-
-                    col[j]=1;
-
+                if(matrix[i][j]==1){
+                    RowVector[i] = 1;
+                    ColVector[j] = 1;
                 }
-
             }
-
         }
-
-        for(int i=0;i<n1;i++)
-
+        
+        
+        for(int i=0;i<r;i++)
         {
-
-            for(int j=0;j<n2;j++)
-
-            {
-
-                if(row[i]==1||col[j]==1)
-
+            if(RowVector[i]==1){
+                
+                for(int j=0;j<c;j++)
                 {
-
-                    m[i][j]=1;
-
+                    matrix[i][j] = 1;
                 }
-
             }
-            
         }
-            
-      
+        for(int j=0;j<c;j++)
+        {
+            if(ColVector[j]==1){
+                
+                for(int i=0;i<r;i++)
+                {
+                    matrix[i][j] = 1;
+                }
+            }
+        }
     }
 };
 
