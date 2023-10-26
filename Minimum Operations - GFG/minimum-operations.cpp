d514c244b0f9b9cabfa880dd2b63dfd652007f9e
Ritik Yadav <96940755+Ritik-Yadav07@users.noreply.github.com>
2023-10-26 13:33:56 +0530

    Added solution - LeetHub

--- Minimum Operations - GFG/minimum-operations.cpp
+++ Minimum Operations - GFG/minimum-operations.cpp
@@ -9,22 +9,34 @@ class Solution
     int minOperation(int n)
     {
         //code here.
-        vector<int>dp(n+1);
-        dp[0]=0;
-        dp[1] = 1;
+        // vector<int>dp(n+1);
+        // dp[0]=0;
+        // dp[1] = 1;
         
-        for(int i=2;i<=n;i++)
-        {
+        // for(int i=2;i<=n;i++)
+        // {
+        //     if(i%2!=0){
+        //         //i is odd
+        //         dp[i] = 1 + dp[i-1];
+        //     }
+        //     else{
+        //         //i is even
+        //         dp[i] = 1 + min(dp[i-1], dp[i/2]);
+        //     }
+        // }
+        // return dp[n];
+        
+        int i = n;
+        int steps = 0;
+        while(i){
             if(i%2!=0){
                 //i is odd
-                dp[i] = 1 + dp[i-1];
+                i--;
             }
-            else{
-                //i is even
-                dp[i] = 1 + min(dp[i-1], dp[i/2]);
-            }
+            else i/=2;
+            steps++;
         }
-        return dp[n];
+        return steps;
     }
 };
 
