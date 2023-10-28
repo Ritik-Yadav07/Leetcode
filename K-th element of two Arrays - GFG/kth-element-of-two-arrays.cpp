b4c64f405054d8d2c10fe4a975700aeaec599708
Ritik Yadav <96940755+Ritik-Yadav07@users.noreply.github.com>
2023-10-28 15:53:09 +0530

    Added solution - LeetHub

--- K-th element of two Arrays - GFG/kth-element-of-two-arrays.cpp
+++ K-th element of two Arrays - GFG/kth-element-of-two-arrays.cpp
@@ -8,31 +8,68 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {  
-      vector<int>arr3;
-       int i=0;
-       int j=0;
-       while(i<n && j<m)
-       {
-           if(arr1[i]<arr2[j]){
-                arr3.push_back(arr1[i]);
+    //   vector<int>arr3;
+    //   int i=0;
+    //   int j=0;
+    //   while(i<n && j<m)
+    //   {
+    //       if(arr1[i]<arr2[j]){
+    //             arr3.push_back(arr1[i]);
+    //             i++;
+    //       }
+    //       else {
+    //           arr3.push_back(arr2[j]);
+    //           j++;
+    //       }
+    //   }
+       
+    //   while(i<n){
+    //       arr3.push_back(arr1[i]);
+    //       i++;
+    //   }
+    //   while(j<m){
+    //       arr3.push_back(arr2[j]);
+    //       j++;
+    //   }
+       
+    //   return arr3[k-1];
+        int i=0,j=0;
+        int cnt = 0;
+        int ele = -1;
+        while(i<n && j<m)
+        {
+            if(arr1[i]<arr2[j])
+            {
+                ele = arr1[i];
+                cnt++;
                 i++;
-           }
-           else {
-              arr3.push_back(arr2[j]);
-               j++;
-           }
-       }
-       
-       while(i<n){
-          arr3.push_back(arr1[i]);
-           i++;
-       }
-       while(j<m){
-          arr3.push_back(arr2[j]);
-           j++;
-       }
-       
-       return arr3[k-1];
+                if(cnt==k) return ele;
+            }
+            else{
+                ele = arr2[j];
+                cnt++;
+                j++;
+                if(cnt==k) return ele;
+            }
+        }
+        
+        while(i<n)
+        {
+             ele = arr1[i];
+             cnt++;
+             i++;
+             if(cnt==k) return ele;
+        }
+        
+        while(j<m)
+        {
+            ele = arr2[j];
+            cnt++;
+            j++;
+            if(cnt==k) return ele;
+        }
+        
+        return ele;
     }
 };
 
